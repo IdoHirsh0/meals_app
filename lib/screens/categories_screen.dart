7613db1283c4d23ab45ba4cf73250a1631ab8696
ido <ido.hirsh.microsoft@gmail.com>
2021-01-02 19:19:35 +0200

    ver1.0

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(
+            title: category.title,
+            color: category.color,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
